9fbaff0192b0efed8ca529553614e1d74fcca352
AlaminRonya <hprony12@gmail.com>
2022-12-10 23:39:33 +0600

    How to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FaComponent } from './fa/fa.component';
+import { FbComponent } from './fb/fb.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: FirstComponent,
+    children: [
+      {
+        path: 'fa-a', component: FaComponent
+      },{
+        path: 'fb-b', component: FbComponent
+      }
+    ]
+  },{
+    path: 's', component: SecondComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { FaComponent } from './fa/fa.component';
+import { FbComponent } from './fb/fb.component';
+
+// const routes: Routes = [
+//   {
+//     path: '', component: FirstComponent,
+//     children: [
+//       {
+//         path: 'fa-a', component: FaComponent
+//       },{
+//         path: 'fb-b', component: FbComponent
+//       }
+//     ]
+//   },{
+//     path: 's', component: SecondComponent
+//   }
+// ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent
+    FirstComponent,
+    SecondComponent,
+    FaComponent,
+    FbComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
